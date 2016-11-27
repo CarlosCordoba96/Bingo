bc2574a43d986cab2e56df4648903d95d42d254c
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2016-11-27 18:28:16 +0100

    Pruebas para juanma

--- Bingo/src/Bingo.java
+++ Bingo/src/Bingo.java
@@ -1,9 +1,29 @@
+import java.util.Random;
 
 public class Bingo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Random a=new Random();
+		int arr [] = new int[15];
+		
+		for(int i=0;i<arr.length;i++){
+			arr[i]=a.nextInt(10);
+		}
+		System.out.println(arr.length);
+		int four=recorrer(arr);
+		System.out.println("The number 4 is repeated : "+four+" times");
+	}
+	
+	
+	private static int recorrer(int [] a){
+		int four=0;
+		for(int i=0;i<a.length;i++){
+			System.out.print(a[i] +" ");
+			
+		}
+		System.out.println();
+		return four;
 	}
 
 }
